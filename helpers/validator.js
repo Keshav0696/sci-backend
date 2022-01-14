f421a2f12623508800b00c1147e9c7ec743a4850
Keshav0696 <keshavgarg0696@gmail.com>
2022-01-14 20:28:29 +0530

    student add

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,7 +1,7 @@
 
 module.exports.validateMobileNumber = function(number){
-    const regex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
-  return regex.test(String(number));
+  const regexExp = /^[6-9]\d{9}$/gi;
+  return regexExp.test(String(number));
 }
 module.exports.validateEmail = function(email) {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
