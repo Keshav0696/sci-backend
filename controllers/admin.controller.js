908e9fcfd45536eb0967c5c5df55073cb02d1e4d
Keshav0696 <keshavgarg0696@gmail.com>
2022-01-14 20:21:02 +0530

    student add

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 const User = mongoose.model('user');
-
+const validator = require("../helpers/validator")
 module.exports.addStudent = async (req, res) => {
     try {
         if (req.user && req.user.role == 'admin') {
             let { name, role, phone_no: phone_number, email} = req.body;
+            if(!validator.validateMobileNumber(phone_no)){
+                return res.status(400).send({ status: 400, message: `Invalid phone_no` })
+            }
+            if(!validator.validateEmail(email)){
+                return res.status(400).send({ status: 400, message: `Invalid email` })
+            }
             let body = { name, role,  phone_number, email};
             for (var p in body) {
                 if (!body[p]) {
---
+++ helpers/validator.js
@@ -0,0 +1,13 @@
+
+module.exports.validateMobileNumber = function(number){
+    const regex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
+  return regex.test(String(number));
+}
+module.exports.validateEmail = function(email) {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+  };
+  module.exports.validateName = function(name) {
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(String(name));
+  };
